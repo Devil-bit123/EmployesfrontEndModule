98df181cc6fa07cecc23a521b5928e14633e2a5c
miguel000127@gmail.com <miguel000127@gmail.com>
2024-05-29 14:47:09 -0500

    # config employe interface
    dsc: add configuration to employes interface

--- src/app/Interfaces/Employes/employes-interface.ts
+++ src/app/Interfaces/Employes/employes-interface.ts
@@ -1,2 +1,10 @@
 export interface EmployesInterface {
+  id: number;
+  name: string | null;
+  lastName: string | null;
+  email: string | null;
+  pay: number | null;
+  contractDate: string | null;
+  idDept: number | null;
+  nameDept: string | null;
 }
