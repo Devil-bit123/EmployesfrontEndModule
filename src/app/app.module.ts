e5dda0cc31ec2104a079fb2a6f02f0ec2bee84c5
miguel000127@gmail.com <miguel000127@gmail.com>
2024-05-29 17:22:24 -0500

    # import material modules
    dsc: import material components to module
    
    # intall dayJS
    
    dsc: install dayJS for date aprsing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,49 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+//Material components
+import {MatButtonModule} from '@angular/material/button';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { NativeDateModule } from '@angular/material/core';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
+//ReactiveForms
+import { ReactiveFormsModule } from '@angular/forms';
+
+//Solicitudes Http
+import { HttpClientModule } from '@angular/common/http';
+
+//dayJS
+import * as dayjs from 'dayjs'
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    //Material
+    MatButtonModule,MatPaginatorModule,MatTableModule,ReactiveFormsModule,
+    MatInputModule,MatSelectModule,MatDatepickerModule,NativeDateModule,
+    MatSnackBarModule,MatIconModule,MatDialogModule,MatGridListModule,
+
+    //Http
+    HttpClientModule,
+
+
+
+
   ],
   providers: [
     provideAnimationsAsync()
