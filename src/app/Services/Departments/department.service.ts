8e3c118fced6ac2221a0e9578301e04dc8111c3d
miguel000127@gmail.com <miguel000127@gmail.com>
2024-05-29 17:04:41 -0500

    # add department services
    dsc: add department service file and configurate get request
    
    # add employe services
    
    dsc: add employe service file and configurate crud requests

--- src/app/Services/Departments/department.service.ts
+++ src/app/Services/Departments/department.service.ts
@@ -1,9 +1,25 @@
+
 import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { DepartmentsInterface } from '../../Interfaces/Departments/departments-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
 
-  constructor() { }
+  private readonly baseUrl = environment.baseURL;
+  private urlAPI = this.baseUrl+"departments/";
+
+  constructor( private http: HttpClient) { }
+
+  getAllDepts():Observable<DepartmentsInterface>{
+    return this.http.get<DepartmentsInterface>(`${this.urlAPI}list`);
+  }
+
+
+
 }
--- src/app/Services/Employes/employe.service.ts
+++ src/app/Services/Employes/employe.service.ts
@@ -1,9 +1,36 @@
+import { EmployesInterface } from './../../Interfaces/Employes/employes-interface';
 import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeService {
 
-  constructor() { }
+  constructor( private http:HttpClient) { }
+
+  private readonly baseUrl = environment.baseURL;
+  private urlAPI = this.baseUrl+"departments/";
+
+  getAllEmployes():Observable<EmployesInterface>{
+    return this.http.get<EmployesInterface>(`${this.urlAPI}list`);
+  }
+
+ addEmploye(model:EmployesInterface):Observable<EmployesInterface>{
+  return  this.http.post<EmployesInterface>(`${this.urlAPI}save`,model);
+ }
+
+ updateEmploye(idEmploye:number,model:EmployesInterface):Observable<EmployesInterface>{
+  return  this.http.put<EmployesInterface>(`${this.urlAPI}update/{idEmploye}`,model);
+ }
+
+ deleteEmploye(idEmploye:number):Observable<void>{
+  return  this.http.delete<void>(`${this.urlAPI}delete/{idEmploye}`);
+ }
+
+
 }
