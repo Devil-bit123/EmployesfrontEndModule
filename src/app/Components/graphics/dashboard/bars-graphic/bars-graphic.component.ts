cc3a1ff42646652d2369b21da09b4c4c6f062c91
miguel000127@gmail.com <miguel000127@gmail.com>
2024-06-04 11:30:28 -0500

    # add bars-graphic compoenent
    dsc: add component to manage bars graphics

---
+++ src/app/Components/graphics/dashboard/bars-graphic/bars-graphic.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bars-graphic',
+  templateUrl: './bars-graphic.component.html',
+  styleUrl: './bars-graphic.component.css'
+})
+export class BarsGraphicComponent {
+
+}
